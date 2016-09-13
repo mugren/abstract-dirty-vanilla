3a1f297b88688324cfd5ee132f92f030778a127e
mugren <igorkivor@gmail.com>
2016-09-13 16:18:00 +0200

    example

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,19 @@
+import apstract.dirty.CrystalEntity
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        def crystalEntity = CrystalEntity.findByName("crystal_entity")?: new CrystalEntity(
+                name: "crystal_entity", location: "Mars")
+
+        crystalEntity.save(flush: true)
+
+        crystalEntity.name = "crystal_entity2"
+        println crystalEntity.dirtyPropertyNames
+        crystalEntity.location = "Jupiter"
+        println crystalEntity.dirtyPropertyNames
+
     }
     def destroy = {
     }
